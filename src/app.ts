88a757054da72e80ce6ec192704c053c09dde592
Vitaly Rtishchev <rtivital@gmail.com>
2019-05-10 21:47:40 +0300

    Setup canvas rendering with store

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import Canvas from './Canvas';
 import backgroundRenderer from './renders/backgroundRenderer';
+import store from './store/store';
 
 const canvas = new Canvas({
   selector: '#app',
   renderers: [backgroundRenderer],
 });
 
+store.subscribe(canvas.render);
 canvas.render();
