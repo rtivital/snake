353a634b27f9bdef701dc4fa4efc5ee5bbbf7c3c
Vitaly Rtishchev <rtivital@gmail.com>
2019-05-11 13:11:28 +0300

    Add score rendering from store

--- src/renders/scoreRenderer.ts
+++ src/renders/scoreRenderer.ts
@@ -1,8 +1,9 @@
 import oc from '../open-color';
 import Canvas from '../Canvas';
+import store from '../store/store';
 
 export default function scoreRenderer({ ctx }: Canvas): void {
   ctx.font = "20px 'Roboto', monospase";
   ctx.fillStyle = oc.white;
-  ctx.fillText('Score: ', 500, 500);
+  ctx.fillText(`Score: ${store.getState().score}`, 500, 500);
 }
