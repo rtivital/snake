f7b4050c2f0ca9b84a6cd6c694a018e832ce5bad
Vitaly Rtishchev <rtivital@gmail.com>
2019-05-11 15:44:12 +0300

    Add direction module

--- src/store/modules/direction.ts
+++ src/store/modules/direction.ts
@@ -1,1 +1,23 @@
 export type DirectionState = 'left' | 'right' | 'down' | 'up';
+
+export const SET_DIRECTION = 'direction/SET_DIRECTION';
+
+interface SetDirectionAction {
+  type: typeof SET_DIRECTION;
+  payload: DirectionState;
+}
+
+export type DirectionActions = SetDirectionAction;
+
+export const setDirection = (direction: DirectionState): SetDirectionAction => ({
+  type: SET_DIRECTION,
+  payload: direction,
+});
+
+export default function directionReducer(state: DirectionState = 'up', action: DirectionActions): DirectionState {
+  if (action.type === SET_DIRECTION) {
+    return action.payload;
+  }
+
+  return state;
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,5 +2,6 @@ import { createStore, combineReducers } from 'redux';
 import getReduxDevTools from './getReduxDevTools';
 import score from './modules/score';
 import snake from './modules/snake';
+import direction from './modules/direction';
 
-export default createStore(combineReducers({ score, snake }), undefined, getReduxDevTools());
+export default createStore(combineReducers({ score, snake, direction }), undefined, getReduxDevTools());
