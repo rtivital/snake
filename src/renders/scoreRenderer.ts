1f966194bad0c49ec898a1d5058ca74e4720fed7
Vitaly Rtishchev <rtivital@gmail.com>
2019-05-12 13:49:05 +0300

    Fix score rendering position

--- src/renders/scoreRenderer.ts
+++ src/renders/scoreRenderer.ts
@@ -1,9 +1,14 @@
 import oc from '../open-color';
 import Canvas from '../Canvas';
 import store from '../store/store';
+import { TILE_SIZE, PLAYGROUND_SIZE, PLAYGROUND_WALL_SIZE } from '../configuration';
 
-export default function scoreRenderer({ ctx }: Canvas): void {
+export default function scoreRenderer({ ctx, width, height }: Canvas): void {
+  const playgroundSize = TILE_SIZE * PLAYGROUND_SIZE;
+  const playbroundBeginX = (width - playgroundSize) / 2;
+  const playbroundBeginY = (height - playgroundSize) / 2;
+
   ctx.font = "20px 'Roboto', monospase";
   ctx.fillStyle = oc.white;
-  ctx.fillText(`Score: ${store.getState().score}`, 500, 500);
+  ctx.fillText(`Score: ${store.getState().score}`, playbroundBeginX - PLAYGROUND_WALL_SIZE, playbroundBeginY - 25);
 }
