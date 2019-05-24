4a03df88896c759e58eb988f62ecb00069246e0c
Vitaly Rtishchev <rtivital@gmail.com>
2019-05-24 14:06:04 +0300

    Move all game logic to separate class

---
+++ src/Game.ts
@@ -0,0 +1,60 @@
+import { GAME_SPEED } from './configuration';
+import store from './store/store';
+import { setDirection, reset, gameOver, initializeGame, incrementScore } from './store/modules/game';
+import { moveSnake, growSnake } from './store/modules/snake';
+import { generateBait } from './store/modules/bait';
+import hasCollisions from './utils/hasCollisions';
+import audio from './audio/audio';
+
+export default class Game {
+  private interval: number = 0;
+
+  private handleKeyDown = (event: KeyboardEvent): void => {
+    const { which } = event;
+    const { game } = store.getState();
+
+    // prevent snake from changing direction during initialization and game over
+    if (!game.gameOver || !game.initialized) {
+      (which === 87 || which === 38) && store.dispatch(setDirection('up'));
+      (which === 83 || which === 40) && store.dispatch(setDirection('down'));
+      (which === 68 || which === 39) && store.dispatch(setDirection('right'));
+      (which === 65 || which === 37) && store.dispatch(setDirection('left'));
+    } else if (which === 32) {
+      if (game.initialized) {
+        this.restart();
+      } else {
+        this.init();
+      }
+    }
+  };
+
+  public init = (): void => {
+    window.addEventListener('keydown', this.handleKeyDown);
+    store.dispatch(initializeGame());
+    this.interval = window.setInterval(this.run, GAME_SPEED);
+  };
+
+  private run = (): void => {
+    const { game, snake, bait } = store.getState();
+
+    if (hasCollisions(snake)) {
+      store.dispatch(gameOver());
+      window.clearInterval(this.interval);
+      audio.gameOver.play();
+    } else {
+      const head = snake[0];
+      if (head.x === bait.x && head.y === bait.y) {
+        store.dispatch(growSnake());
+        store.dispatch(generateBait(snake));
+        store.dispatch(incrementScore());
+        audio.bait.play();
+      }
+      store.dispatch(moveSnake(game.direction));
+    }
+  };
+
+  private restart = () => {
+    store.dispatch(reset());
+    this.interval = window.setInterval(this.run, GAME_SPEED);
+  };
+}
--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,7 @@ import snakeRenderer from './renders/snakeRenderer';
 import baitRenderer from './renders/baitRenderer';
 import gameOverRenderer from './renders/gameOverRenderer';
 import store from './store/store';
-import { moveSnake, growSnake } from './store/modules/snake';
-import { GAME_SPEED } from './configuration';
-import { gameOver, setGameInterval, incrementScore, setDirection } from './store/modules/game';
-import audio from './audio/audio';
-import hasCollisions from './utils/hasCollisions';
-import { generateBait } from './store/modules/bait';
+import Game from './Game';
 
 const canvas = new Canvas({
   selector: '#app',
@@ -21,47 +16,4 @@ const canvas = new Canvas({
 store.subscribe(canvas.render);
 canvas.render();
 
-window.addEventListener('keydown', (event) => {
-  const { which } = event;
-
-  if (which === 87 || which === 38) {
-    store.dispatch(setDirection('up'));
-  }
-
-  if (which === 83 || which === 40) {
-    store.dispatch(setDirection('down'));
-  }
-
-  if (which === 68 || which === 39) {
-    store.dispatch(setDirection('right'));
-  }
-
-  if (which === 65 || which === 37) {
-    store.dispatch(setDirection('left'));
-  }
-});
-
-function game(): void {
-  const {
-    snake,
-    bait,
-    game: { interval, direction },
-  } = store.getState();
-
-  if (hasCollisions(snake)) {
-    store.dispatch(gameOver());
-    window.clearInterval(interval);
-    audio.gameOver.play();
-  } else {
-    const head = snake[0];
-    if (head.x === bait.x && head.y === bait.y) {
-      store.dispatch(growSnake());
-      store.dispatch(generateBait(snake));
-      store.dispatch(incrementScore());
-      audio.bait.play();
-    }
-    store.dispatch(moveSnake(direction));
-  }
-}
-
-store.dispatch(setGameInterval(window.setInterval(game, GAME_SPEED)));
+new Game().init();
