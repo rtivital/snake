17b394702fed743d4558430502d94d139401bd3c
Vitaly Rtishchev <rtivital@gmail.com>
2019-05-24 14:23:12 +0300

    Fix redux dev tools in production

--- src/store/getReduxDevTools.ts
+++ src/store/getReduxDevTools.ts
@@ -1,6 +1,6 @@
 import { StoreEnhancerStoreCreator } from 'redux';
 
 export default function getReduxDevTools(): () => StoreEnhancerStoreCreator {
-  const devtoolsEnabled = typeof (window as any).devToolsExtension === 'function' && process.env.NODE_ENV === 'development';
+  const devtoolsEnabled = typeof (window as any).devToolsExtension === 'function';
   return devtoolsEnabled ? (window as any).__REDUX_DEVTOOLS_EXTENSION__() : Function.prototype;
 }
